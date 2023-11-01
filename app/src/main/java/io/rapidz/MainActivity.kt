62cbfb8426ec1d8652ff44777fd9ec0a323f3121
charlesliew <charles.liew@claritytech.sg>
2023-11-01 16:18:36 +0800

    Initial commit

---
+++ app/src/main/java/io/rapidz/MainActivity.kt
@@ -0,0 +1,12 @@
+package io.rapidz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.rapidz.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
