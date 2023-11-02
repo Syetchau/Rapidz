220dc25c87be814d4dc8a8d3caee5474931b6793
charlesliew <charles.liew@claritytech.sg>
2023-11-02 09:46:57 +0800

    edit gradle

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -42,3 +42,13 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            groupId = "com.github.Syetchau.Rapidz"
+            artifactId = "RapidzLib"
+            version = "1.0.0"
+        }
+    }
+}
