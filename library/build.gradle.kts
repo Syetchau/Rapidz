0e7be34493e5070452807fdcc4694913199c7d3c
charlesliew <charles.liew@claritytech.sg>
2023-11-09 11:52:09 +0800

    add comment

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -47,49 +47,58 @@ dependencies {
 //    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
 }
 
+/**
+ * This is for publishing and build AAR in local
+ * Built AAR and commit & push to bitbucket
+ * Comment these lines if publishing to Github, as it will return error and unable to release nxt version
+ */
 artifacts {
     archives(file("$buildDir/outputs/aar/${project.name}-release.aar")) // eg. "$buildDir/outputs/aar/${project.getName()}-release.aar"
 }
 
-//publishing {
-//    publications {
-//        create<MavenPublication>("mavenAar") {
-//            groupId = "com.rapidz"
-//            version = "1.0.2"
-//            artifactId = "android.library"
-//
-//            artifact(file("$buildDir/outputs/aar/${project.name}-release.aar")) // Replace "aar location" with the actual location of your AAR file
-//
-//            pom.withXml {
-//                val dependenciesNode = asNode().appendNode("dependencies")
-//
-//                configurations["implementation"].allDependencies.forEach { dependency ->
-//                    val dependencyNode = dependenciesNode.appendNode("dependency")
-//                    dependencyNode.appendNode("groupId", dependency.group)
-//                    dependencyNode.appendNode("artifactId", dependency.name)
-//                    dependencyNode.appendNode("version", dependency.version)
-//                }
-//            }
-//        }
-//
-//        repositories {
-//            maven {
-//                name = "android-rapidz-library"
-//                setUrl( "/Users/charlesliew/StudioProjects/Rapidz/") // location where build generated
-//            }
-//        }
-//    }
-//}
-
-
-// This is for publish to Github
 publishing {
     publications {
-        create<MavenPublication>("ReleaseAar") {
-            groupId = "com.github.Syetchau.Rapidz"
-            artifactId = "RapidzLib"
-            version = "1.0.1"
-            afterEvaluate { artifact(tasks.getByName("bundleReleaseAar")) }
+        create<MavenPublication>("mavenAar") {
+            groupId = "com.rapidz"
+            version = "1.0.2"
+            artifactId = "android.library"
+
+            artifact(file("$buildDir/outputs/aar/${project.name}-release.aar")) // Replace "aar location" with the actual location of your AAR file
+
+            pom.withXml {
+                val dependenciesNode = asNode().appendNode("dependencies")
+
+                configurations["implementation"].allDependencies.forEach { dependency ->
+                    val dependencyNode = dependenciesNode.appendNode("dependency")
+                    dependencyNode.appendNode("groupId", dependency.group)
+                    dependencyNode.appendNode("artifactId", dependency.name)
+                    dependencyNode.appendNode("version", dependency.version)
+                }
+            }
+        }
+
+        repositories {
+            maven {
+                name = "android-rapidz-library"
+                setUrl( "/Users/charlesliew/StudioProjects/Rapidz/") // location where build generated
+            }
         }
     }
 }
+
+/**
+ * This is for publish to Github, uncomment if using for publishing to bitbucket
+ * Check the versioning of released AAR at jitpack.io
+ * Red = failed, green = success
+ * Released AAR to jitpack.io might taking time, so the version increment is not instant
+ */
+//publishing {
+//    publications {
+//        create<MavenPublication>("ReleaseAar") {
+//            groupId = "com.github.Syetchau.Rapidz"
+//            artifactId = "RapidzLib"
+//            version = "1.0.1"
+//            afterEvaluate { artifact(tasks.getByName("bundleReleaseAar")) }
+//        }
+//    }
+//}
